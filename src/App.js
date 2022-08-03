568bafb0cecb4cfa424d95fc0677738c3bea01ed
Sidnei Rafael Biz <sidneibiz@gmail.com>
2022-08-03 23:22:43 +0000

    Criado divisao de telas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LogoSvg from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="app">
+      <div class="container2">
+        <div class="conteudo">1</div>
+        <div class="conteudo">2</div>
+        <div class="conteudo">3</div>
+        <div class="conteudo">4</div>
+      </div>
+      <div class="container">
+        <div class="logo">
+          <img src={LogoSvg} alt="Logo"></img>
+        </div>
+        <div class="conteudo">
+          <div class="desc">Aqui vou escrever um conteudo</div>
+        </div>
+      </div>
+      <div class="container">
+        <div>Acabou</div>
+      </div>
     </div>
   );
 }
